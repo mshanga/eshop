d4caea5430f8ea25fe8d72826f58a7d2cfaf6967
Mshanga Makonde <mshangamakonde@gmail.com>
2018-10-03 17:58:12 +0300

    release.js

--- debug.js
+++ debug.js
@@ -6,11 +6,11 @@
 
 const options = {};
 
-options.ip = '127.0.0.1';
-options.port = parseInt(process.argv[2]);
-options.config = { name: 'Total.js' };
-options.sleep = 3000;
-options.inspector = 9229;
-options.watch = ['private'];
+// options.ip = '127.0.0.1';
+// options.port = parseInt(process.argv[2]);
+// options.config = { name: 'Total.js' };
+// options.sleep = 3000;
+// options.inspector = 9229;
+// options.watch = ['private'];
 
 require('total.js/debug')(options);
